ef8722788dccbc553543b823f95ef436195140d0
randi <randeggu@stud.ntnu.no>
2019-11-07 12:01:40 +0100

    testing CI - adding client

--- DaoServer/src/sql_script_english.sql
+++ DaoServer/src/sql_script_english.sql
@@ -5,8 +5,8 @@ Drop table if exists User;
 
 
 CREATE table User(
-  username varchar(30) primary key ,
-  password varchar(300)
+                   username varchar(30) primary key ,
+                   password varchar(300)
 );
 
 CREATE table Category(
@@ -14,27 +14,27 @@ CREATE table Category(
 );
 
 CREATE table Article(
-                  id INT AUTO_INCREMENT PRIMARY KEY ,
-                  title VARCHAR(30) NOT NULL ,
-                  text LONGTEXT NOT NULL,
-                  creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                  last_edited TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                  image varchar(10000),
-                  alt varchar(50),
-                  category varchar(30),
-                  importance boolean,
-                  image_text varchar(50),
-                  creator varchar(30),
-                  CONSTRAINT foreign key (creator) REFERENCES User(username),
-                  CONSTRAINT foreign key (category) REFERENCES Category(category)
+                      id INT AUTO_INCREMENT PRIMARY KEY ,
+                      title VARCHAR(30) NOT NULL ,
+                      text LONGTEXT NOT NULL,
+                      creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      last_edited integer,
+                      image varchar(10000),
+                      alt varchar(50),
+                      category varchar(30),
+                      importance boolean,
+                      image_text varchar(50),
+                      creator varchar(30),
+                      CONSTRAINT foreign key (creator) REFERENCES User(username),
+                      CONSTRAINT foreign key (category) REFERENCES Category(category)
 );
 
 CREATE table Comment(
-                       id INT AUTO_INCREMENT PRIMARY KEY ,
-                       text TEXT,
-                       creation_date TIMESTAMP DEFAULT current_timestamp,
-                     creator varchar(30),
-                     article int,
-                       CONSTRAINT foreign key (creator) REFERENCES User(username),
-                       CONSTRAINT foreign key (article) REFERENCES Article(id)
+                      id INT AUTO_INCREMENT PRIMARY KEY ,
+                      text TEXT,
+                      creation_date TIMESTAMP DEFAULT current_timestamp,
+                      creator varchar(30),
+                      article int,
+                      CONSTRAINT foreign key (creator) REFERENCES User(username),
+                      CONSTRAINT foreign key (article) REFERENCES Article(id)
 );
